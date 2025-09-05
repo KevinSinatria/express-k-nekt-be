af3c8becdd1a6e65c82ed458d07ccb5b749f43cc
KevinSinatria <kevinsinatria8@gmail.com>
2025-09-05 08:56:11 +0700

    feat(auth): login controller completed

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,8 @@
 import express from "express";
-import { helloWorld } from "../controllers/index.js";
 import authRoutes from "./auth/index.js";
 
 const router = express.Router();
 
-// router.get("/", helloWorld);
 router.use("/auth", authRoutes);
 
 export default router;
