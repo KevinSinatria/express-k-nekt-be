f50ce509eabda7cb9efe9c1909e974ca66d0472e
KevinSinatria <kevinsinatria8@gmail.com>
2025-09-05 14:21:14 +0700

    feat(violations): violations feature completed

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import authRoutes from "./auth/index.js";
+import violationsRoutes from "./violations/index.js";
+import { authenticate } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
+router.use("/violations", authenticate, violationsRoutes);
 
 export default router;
