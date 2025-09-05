6eda90ccb3bf9c958883a5bbec031e05d381c34c
KevinSinatria <kevinsinatria8@gmail.com>
2025-09-05 18:45:40 +0700

    feat(students): students feature has completed

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import authRoutes from "./auth/index.js";
 import violationsRoutes from "./violations/index.js";
+import studentRoutes from "./students/index.js";
 import { authenticate } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/violations", authenticate, violationsRoutes);
+router.use("/students", authenticate, studentRoutes);
 
 export default router;
